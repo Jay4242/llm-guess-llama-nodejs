10242209db4d577f9ee62103eecb7c3f2768530e
Jay4242 <96741732+Jay4242@users.noreply.github.com>
2026-09-01 21:07:00 -0700

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: "client",
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [""],
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
+});
